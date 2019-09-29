e3746306ba94000e54477f60cf3938f1d2ed511d
klong-knn <54529534+klong-knn@users.noreply.github.com>
2019-09-30 00:03:27 +0700

    ASS7_SEC1_603020336-1
    29/9/2562

---
+++ app/src/main/java/com/example/ass7_sec_603020336_1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ass7_sec_603020336_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ass7.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
